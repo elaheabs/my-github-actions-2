46b9a47c08cf6e0f14666d4278d6a689ac2e73f7
elaheabs <elahe.abs@gmail.com>
2022-06-22 10:09:33 -0300

    120

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 13
+          Learn React 15
         </a>
       </header>
     </div>
