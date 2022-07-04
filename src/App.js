4fe699f415e04b4d2788cbab788054afb4aadb46
elaheabs <elahe.abs@gmail.com>
2022-07-04 10:22:47 -0300

    fix: fixed the text to ticket4

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React ticket3 + a prefix
+          Learn React ticket4
         </a>
       </header>
     </div>
